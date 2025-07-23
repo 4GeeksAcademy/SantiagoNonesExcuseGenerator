373932bb260e6187285f636be2162566d9b304e1
SantiNones <santiagonones@gmail.com>
2025-07-23 12:58:46 +0000

    live

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-import "bootstrap";
-import "./style.css";
+function generateExcuse() {
+  let who = ["El Barbero", "El sobrino de mi tía", "El loco de la cuadra", "Donald J. Trump", "Chespirito"];
+  let action = ["lanzó por la ventana", "se limpió la salsa de la cara con", "cambió por una pepsicola", "perdió", "tiró por la alcantarilla"];
+  let what = ["mis llaves", "el cargador", "el único boxer limpio", "mi billetera", "la computadora"];
+  let when = ["ayer por la noche", "ahoritica mismo", "en plena hora de la siesta", "hace raaaato"];
 
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  const excuse = generateExcuse();
+  document.getElementById("excuse").innerText = excuse;
 };
