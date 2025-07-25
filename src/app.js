34c9da570f12dbb27e7fcd18e0717c1d3dd841c4
SantiNones <santiagonones@gmail.com>
2025-07-25 09:56:49 +0000

    Random Excuse Generator

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 function generateExcuse() {
   let who = ["El Barbero", "El sobrino de mi tía", "El loco de la cuadra", "Donald J. Trump", "Chespirito"];
   let action = ["lanzó por la ventana", "se limpió la salsa de la cara con", "cambió por una pepsicola", "perdió", "tiró por la alcantarilla"];
-  let what = ["mis llaves", "el cargador", "el único boxer limpio", "mi billetera", "la computadora"];
+  let what = ["mis llaves", "mi cargador", "mi único boxer limpio", "mi billetera", "mi computadora"];
   let when = ["ayer por la noche", "ahoritica mismo", "en plena hora de la siesta", "hace raaaato"];
 
   let randomWho = who[Math.floor(Math.random() * who.length)];
